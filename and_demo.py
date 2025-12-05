f1bb6fe8ed8c61de7a6cc80ac0c5b86653490557
Nils-nonline <nils-nonline@web.de>
2025-12-05 22:06:23 +0100

    added copyright notice

--- and_demo.py
+++ and_demo.py
@@ -3,6 +3,12 @@ from deep_learning import Perceptron, Layer, Model
 import matplotlib.pyplot as plt
 import statistics
 
+"""
+Copyright (c) 2025 Nils Häußler. All Rights Reserved.
+
+This work may not be copied, reproduced, distributed, displayed, performed, modified, adapted, published, transmitted, or used to create derivative works in any form or by any means without the express prior written permission of the copyright owner. No rights are granted to any user except for the right to view the work for personal reference or rights explicitly granted by law. All other rights are strictly reserved to the author.
+"""
+
 
 a = Perceptron(num_weights=3,aktivation_fun="sigmoid",loss_func="linear")
 
--- xor_demo.py
+++ xor_demo.py
@@ -3,6 +3,11 @@ from deep_learning import Perceptron, Layer, Model
 import matplotlib.pyplot as plt
 import statistics
 
+"""
+Copyright (c) 2025 Nils Häußler. All Rights Reserved.
+
+This work may not be copied, reproduced, distributed, displayed, performed, modified, adapted, published, transmitted, or used to create derivative works in any form or by any means without the express prior written permission of the copyright owner. No rights are granted to any user except for the right to view the work for personal reference or rights explicitly granted by law. All other rights are strictly reserved to the author.
+"""
 
 model = Model(input_size = 2, random_seed=3)
 
